10b1f813f53fcdbe66b8cbe76aff9a04ea540585
Gustav Ung <guaschman@gmail.com>
2019-06-09 21:40:05 +0200

    implements a simple parsing for lambda expressions

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
-//#[macro_use]
+#[macro_use]
 extern crate nom;
 
 use std::fs;
 
-use nom::{
-    IResult,
-    bytes::complete::tag
-};
+use nom::IResult;
 
 #[derive(Debug)]
 enum Expr {
@@ -18,13 +15,27 @@ enum Expr {
     If { e1: Box<Expr>, e2: Box<Expr>, e3: Box<Expr> }
 }
 
-fn abs(input: &str) -> IResult<&str, Expr> {
-    let (input, _) = nom::bytes::complete::tag("\\", input)?;
-    Ok ((input, Expr::Var { name: "".to_string() }))
-}
+named!(lambda(&str) -> Expr, do_parse!(
+        tag!(r#"\"#) >>
+        name: take_until_and_consume!(".") >>
+        expr: expression >>
+        (Expr::Lam { name: name.to_string(), e: Box::new(expr) })
+    )
+);
+
+named!(var(&str) -> Expr, do_parse!(
+        eat_separator!(" \r\t\n") >>
+        name: take_until_either_and_consume!(" \r\t\n") >>
+        (Expr::Var { name: name.to_string()})
+    )
+ );
+
+named!(expression(&str) -> Expr, alt!(
+         var | lambda
+    )
+);
 
 fn main() {
     let data = fs::read_to_string("test.hm").expect("Unable to find file");
-    println!("{:?}", abs(data.as_str()));
-    println!("Hello, world!");
+    println!("{:?}", lambda(data.as_str()));
 }

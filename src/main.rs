49e0dcac17dff45c450808318fc8f08dcd5c343c
Gustav Ung <guaschman@gmail.com>
2019-06-09 22:16:39 +0200

    Implement a first attempt at parsing

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ extern crate nom;
 
 mod parser;
 
-use parser::lambda;
+use parser::expression;
 
 use std::fs;
 
 fn main() {
     let data = fs::read_to_string("test.hm").expect("Unable to find file");
-    println!("{:?}", lambda(data.as_str()));
+    println!("{:?}", expression(data.as_str()));
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,14 +1,55 @@
 #[derive(Debug)]
+pub enum Literal {
+    Bool(bool),
+    Int(u32)
+}
+
+#[derive(Debug)]
 pub enum Expr {
     Var(String),
     App(Box<Expr>, Box<Expr>),
     Lam(String, Box<Expr>),
     Let(String, Box<Expr>, Box<Expr>),
-    Lit(u32),
+    Lit(Literal),
     If(Box<Expr>, Box<Expr>, Box<Expr>)
 }
 
-named!(pub lambda(&str) -> Expr, do_parse!(
+named!(pub expression(&str) -> Expr, alt!(
+        let_expr | if_expr | lambda_expr | var_expr | app_expr
+    )
+);
+
+named!(pub let_expr(&str) -> Expr, do_parse!(
+        eat_separator!(" \r\t\n") >>
+        tag!("let") >>
+        eat_separator!(" \r\t\n") >>
+        name: take_until_and_consume!(" ") >>
+        eat_separator!(" \r\t\n") >>
+        expr1: expression >>
+        eat_separator!(" \r\t\n") >>
+        expr2: expression >>
+        ( Expr::Let(name.to_string(), Box::new(expr1), Box::new(expr2)) )
+    )
+);
+
+named!(pub if_expr(&str) -> Expr, do_parse!(
+        eat_separator!(" \r\t\n") >>
+        tag!("if") >>
+        eat_separator!(" \r\t\n") >>
+        expr1: expression >>
+        eat_separator!(" \r\t\n") >>
+        tag!("then") >>
+        eat_separator!(" \r\t\n") >>
+        expr2: expression >>
+        eat_separator!(" \r\t\n") >>
+        tag!("else") >>
+        eat_separator!(" \r\t\n") >>
+        expr3: expression >>
+        ( Expr::If(Box::new(expr1), Box::new(expr2), Box::new(expr3)) )
+    )
+);
+
+named!(pub lambda_expr(&str) -> Expr, do_parse!(
         tag!(r#"\"#) >>
         name: take_until_and_consume!(".") >>
         expr: expression >>
@@ -16,14 +57,18 @@ named!(pub lambda(&str) -> Expr, do_parse!(
     )
 );
 
-named!(pub var(&str) -> Expr, do_parse!(
+named!(pub var_expr(&str) -> Expr, do_parse!(
         eat_separator!(" \r\t\n") >>
         name: take_until_either_and_consume!(" \r\t\n") >>
         ( Expr::Var(name.to_string()) )
     )
  );
 
-named!(pub expression(&str) -> Expr, alt!(
-         var | lambda
-    )
-);
+ named!(pub app_expr(&str) -> Expr, do_parse!(
+         eat_separator!(" \r\t\n") >>
+         expr1: expression >>
+         eat_separator!(" \r\t\n") >>
+         expr2: expression >>
+         ( Expr::App(Box::new(expr1), Box::new(expr2)) )
+     )
+ );

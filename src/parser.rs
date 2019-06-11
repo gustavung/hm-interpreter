586227dcd70d09dfab8029df6f901e98af9a1b66
Gustav Ung <guaschman@gmail.com>
2019-06-11 15:00:04 +0200

    Fix let expressions and add proper parsing for integers and booleans

--- src/parser.rs
+++ src/parser.rs
@@ -1,16 +1,19 @@
+use nom::{digit1, alpha};
+
 #[derive(Debug)]
 pub enum Expr {
     Var(String),
     App(Box<Expr>, Box<Expr>),
     Lam(String, Box<Expr>),
     Let(String, Box<Expr>, Box<Expr>),
-    Lit,
+    BoolLit(bool),
+    IntLit(i32),
     If(Box<Expr>, Box<Expr>, Box<Expr>)
 }
 
 // TODO: handle integer and boolean literals
 named!(pub expression(&str) -> Expr, alt!(
-        let_expr | if_expr | lambda_expr | var_expr | app_expr
+        let_expr | if_expr | lambda_expr | bool_expr | int_expr | var_expr | app_expr
     )
 );
 
@@ -20,7 +23,11 @@ named!(pub let_expr(&str) -> Expr, do_parse!(
         eat_separator!(" \r\t\n") >>
         name: take_until_and_consume!(" ") >>
         eat_separator!(" \r\t\n") >>
+        tag!("=") >>
+        eat_separator!(" \r\t\n") >>
         expr1: expression >>
+        eat_separator!(" \r\t\n") >>
+        tag!("in") >>
         eat_separator!(" \r\t\n") >>
         expr2: expression >>
         ( Expr::Let(name.to_string(), Box::new(expr1), Box::new(expr2)) )
@@ -61,10 +68,25 @@ named!(pub var_expr(&str) -> Expr, do_parse!(
  );
 
 named!(pub app_expr(&str) -> Expr, do_parse!(
-         eat_separator!(" \r\t\n") >>
-         expr1: expression >>
-         eat_separator!(" \r\t\n") >>
-         expr2: expression >>
-         ( Expr::App(Box::new(expr1), Box::new(expr2)) )
-     )
- );
+        eat_separator!(" \r\t\n") >>
+        expr1: expression >>
+        eat_separator!(" \r\t\n") >>
+        expr2: expression >>
+        ( Expr::App(Box::new(expr1), Box::new(expr2)) )
+    )
+);
+
+named!(pub bool_expr(&str) -> Expr, do_parse!(
+        eat_separator!(" \r\t\n") >>
+        b: alt!(tag!("true") => { |_| true } |
+                tag!("false") => { |_| false } ) >>
+        ( Expr::BoolLit(b) )
+    )
+);
+
+named!(pub int_expr(&str) -> Expr, do_parse!(
+        eat_separator!(" \r\t\n") >>
+        i: map_res!(digit1, |integer: &str| integer.parse::<i32>())>>
+        ( Expr::IntLit(i) )
+    )
+);
